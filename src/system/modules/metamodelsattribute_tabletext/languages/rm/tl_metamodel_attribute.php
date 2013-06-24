558fbbea04303b5e84db9cb128e3af5177a49c3d
Mini Model <minimodel@metamodel.me>
2013-06-24 16:17:03 +0200

    Updated translations from transifex.

---
+++ src/system/modules/metamodelsattribute_tabletext/languages/rm/tl_metamodel_attribute.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/rm/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-06-24T12:11:54+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_cols']['0']      = 'Configuraziun da las colonnas';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowLabel']['0']  = 'Label';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowLabel']['1']  = 'Endatescha ils labels per las colonnas';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowStyle']['0']  = 'Ladezza';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowStyle']['1']  = 'Endatescha la ladezza da las colonnas (p.ex. 200px)';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['tabletext'] = 'Tabella da text';
