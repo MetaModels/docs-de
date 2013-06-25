ca60bebc23cb0eaff27833202582de124df77cd9
Mini Model <minimodel@metamodel.me>
2013-06-25 12:17:48 +0200

    Updated translations from transifex.

--- src/system/modules/metamodelsattribute_tabletext/languages/de/tl_metamodel_attribute.php
+++ src/system/modules/metamodelsattribute_tabletext/languages/de/tl_metamodel_attribute.php
@@ -8,8 +8,12 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-06-24T12:11:54+02:00
+ * last-updated: 2013-06-24T16:17:03+02:00
  */
 
-
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowLabel']['0'] = 'Label';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_cols']['0']      = 'Spalteneinstellung';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowLabel']['0']  = 'Label';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowLabel']['1']  = 'Bitte geben Sie die Labels für die Spalten an.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowStyle']['0']  = 'Breite';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowStyle']['1']  = 'Bitte geben Sie die Spaltenbreite ein (beispielsweise 200px).';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['tabletext'] = 'Text-Tabelle';
