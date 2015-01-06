7fcd5f52baafd518ece9798681f42c4b82680c49
Christian Schiffler <c.schiffler@cyberspectrum.de>
2015-01-06 02:07:33 +0100

    disable modindex for HTML.

--- conf.py
+++ conf.py
@@ -36,6 +36,7 @@ html_theme = 'default'
 html_theme = "sphinx_rtd_theme"
 html_theme_path = ["_themes", ]
 html_static_path = ['_static']
+html_use_modindex = False
 htmlhelp_basename = 'MetaModelsdoc'
 
 # -- Options for LaTeX output ---------------------------------------------
