4fef139ddc9f3b7e092577236d2b43b3b58a5800
Mini Model <minimodel@metamodel.me>
2015-09-18 10:34:03 +0200

    Updated translations from transifex.

---
+++ contao/languages/ru/tl_metamodel_attribute.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/ru/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2015-07-11T08:03:18+02:00
+ */
+
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_cols']['0']      = 'Конфигурация колонки';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowLabel']['0']  = 'Метка';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowLabel']['1']  = 'Введите метки для колонок';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowStyle']['0']  = 'Ширина';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowStyle']['1']  = 'Введите ширину колонок (наприм., 200px)';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['tabletext'] = 'Текст таблицы';
+
