cc869dcbd3a546ab53750a1183b1451bcdcfcc6d
Ingolf Steinhardt <info@e-spin.de>
2017-01-01 19:21:55 +0100

    add Link to Footer
    add Link to Footer

--- conf.py
+++ conf.py
@@ -24,7 +24,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 master_doc = 'index'
 project = u'MetaModels'
-copyright = u'{:d}, Team MetaModels'.format(date.today().year)
+copyright = u'{:d}, <a href="https://now.metamodel.me/de/ueber-uns/team" title="Team MetaModels" target="_blank">Team MetaModels</a>'.format(date.today().year)
 version = '2.0'
 release = '2.0.0'
 language = 'de'
