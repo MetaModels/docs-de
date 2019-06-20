722b4ace0d7ebac9176d5f554fe951b85ae52c5d
Mini Model <minimodel@metamodel.me>
2019-06-20 07:20:43 +0200

    Updated translations from transifex.

--- src/Resources/contao/languages/et/tl_metamodel_attribute.php
+++ src/Resources/contao/languages/et/tl_metamodel_attribute.php
@@ -1,19 +1,10 @@
 <?php
 /**
- * Translations are managed using Transifex. To create a new translation
- * or to help to maintain an existing one, please register at transifex.com.
- *
- * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
- *
- * @link https://www.transifex.com/signup/
- * @link https://www.transifex.com/projects/p/$$project$$/language/$$lang$$/
- *
- * last-updated: 2017-08-24T14:39:27+02:00
+
  */
 
 
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tablemulti_cols']['0']      = 'Veergude seadistamine';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tablemulti_rowLabel']['0']  = 'Silt';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tablemulti_rowStyle']['0']  = 'Laius';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['tablemulti'] = 'Multi tabel';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tablemulti_cols']['0']     = 'Veergude seadistamine';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tablemulti_rowLabel']['0'] = 'Silt';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tablemulti_rowStyle']['0'] = 'Laius';
 
